9122d4aed565e771e301a0ce52bde602072a2730
WAYOFFprod <raphael@wayoff.ch>
2022-04-17 15:33:07 +0200

    udpated git ignore

---
+++ src/components/MenuDrawer.vue
@@ -0,0 +1,46 @@
+<template>
+  <n-drawer v-model:show="active" :width="502" placement="bottom">
+      <n-drawer-content title="Colors">
+        <StartMenu 
+          v-if="!store.isLoggedIn && store.isFinishedConnecting"
+          @openRegistration="openRegistration"
+          @openLogin="openLogin"/>
+        <ColorSelector
+          v-if="store.isLoggedIn"
+          class="swatch-container"
+          />
+      </n-drawer-content>
+    </n-drawer>
+</template>
+
+<script>
+import ColorSelector from './ColorSelector.vue'
+import StartMenu from './StartMenu.vue'
+import { store } from './../store.js'
+import { NDrawer, NDrawerContent } from 'naive-ui'
+export default {
+  components: {
+    ColorSelector,
+    StartMenu,
+    NDrawer,
+    NDrawerContent
+  },
+  data () {
+    return {
+      store,
+    }
+  },
+  methods: {
+    openLogin() {
+      this.$refs.loginModal.openLogin()
+    },
+    openRegistration() {
+      this.$refs.loginModal.openRegister()
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ScriptDrawer.vue
@@ -0,0 +1,36 @@
+<template>
+  <n-drawer v-model:show="store.isScriptDrawerOpen" :width="502" placement="top">
+      <n-drawer-content title="Colors">
+        <n-button @click="startScript">
+          RUN
+        </n-button>
+      </n-drawer-content>
+    </n-drawer>
+</template>
+
+<script>
+import { store } from './../store.js'
+import { NDrawer, NDrawerContent, NButton } from 'naive-ui'
+
+export default {
+  components: {
+    NDrawer,
+    NDrawerContent,
+    NButton
+  },
+  data () {
+    return {
+      store,
+    }
+  },
+  methods: {
+    startScript() {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
